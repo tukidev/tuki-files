6725d10237ceec00c61de900220ad926cdd8617e
tukidev <tuky.chyvekshyno@gmail.com>
2022-03-25 15:25:30 +0200

    some view fixes.

--- nvim/.config/nvim/lua/tukivim/plugin-config/ui/init.lua
+++ nvim/.config/nvim/lua/tukivim/plugin-config/ui/init.lua
@@ -6,7 +6,7 @@ local U = require("tukivim.com.source.utils")
 local R = require("tukivim.com.res")
 local PATH = R.path.config.ui
 
-require(PATH.colorschemes.p).set("nightfox")       -- set colorscheme
+require(PATH.colorschemes.p).set("catppuccin")       -- set colorscheme
 
 -- setup modules
 U.req_list(U.path_to_list(PATH.view.p, require(PATH.view.p)))
