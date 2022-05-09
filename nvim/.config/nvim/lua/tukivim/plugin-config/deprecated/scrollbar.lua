9aab24a171970561eb26102dd19668b2f0a477f1
tukidev <tuky.chyvekshyno@gmail.com>
2022-05-09 18:21:26 +0300

    nvim: deprecated: `scrollbar` plugin.

---
+++ nvim/.config/nvim/lua/tukivim/plugin-config/deprecated/scrollbar.lua
@@ -0,0 +1,95 @@
+require("scrollbar").setup({
+    show = true,
+    set_highlights = true,
+    folds = 1000, -- handle folds, set to number to disable folds if no. of lines in buffer exceeds this
+    max_lines = false, -- disables if no. of lines in buffer exceeds this
+    handle = {
+        text = " ",
+        color = nil,
+        cterm = nil,
+        highlight = "CursorLine",
+        hide_if_all_visible = true, -- Hides handle if all lines are visible
+    },
+    marks = {
+        Search = {
+            text = { "-", "=" },
+            priority = 0,
+            color = nil,
+            cterm = nil,
+            highlight = "Search",
+        },
+        Error = {
+            text = { "-", "=" },
+            priority = 1,
+            color = nil,
+            cterm = nil,
+            highlight = "DiagnosticVirtualTextError",
+        },
+        Warn = {
+            text = { "-", "=" },
+            priority = 2,
+            color = nil,
+            cterm = nil,
+            highlight = "DiagnosticVirtualTextWarn",
+        },
+        Info = {
+            text = { "-", "=" },
+            priority = 3,
+            color = nil,
+            cterm = nil,
+            highlight = "DiagnosticVirtualTextInfo",
+        },
+        Hint = {
+            text = { "-", "=" },
+            priority = 4,
+            color = nil,
+            cterm = nil,
+            highlight = "DiagnosticVirtualTextHint",
+        },
+        Misc = {
+            text = { "-", "=" },
+            priority = 5,
+            color = nil,
+            cterm = nil,
+            highlight = "Normal",
+        },
+    },
+    excluded_buftypes = {
+        "terminal",
+        "NvimTree",
+    },
+    excluded_filetypes = {
+        "prompt",
+        "TelescopePrompt",
+        "NvimTree",
+        "nvimtree"
+    },
+    autocmd = {
+        render = {
+            "BufWinEnter",
+            "TabEnter",
+            "TermEnter",
+            "WinEnter",
+            "CmdwinLeave",
+            "TextChanged",
+            "VimResized",
+            "WinScrolled",
+        },
+    },
+    handlers = {
+        diagnostic = true,
+        search = true,
+    },
+})
+
+require('hlslens').build_position_cb = function(plist, _, _, _)
+    require("scrollbar.handlers.search").handler.show(plist.start_pos)
+end
+
+
+vim.cmd([[
+    augroup scrollbar_search_hide
+        autocmd!
+        autocmd CmdlineLeave : lua require('scrollbar.handlers.search').handler.hide()
+    augroup END
+]])
